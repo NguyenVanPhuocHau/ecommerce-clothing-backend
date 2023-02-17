bab99a029e6d9938650c9d1c6a08d430c1a662e0
NguyenVanPhuocHau-19130066 <92570580+NguyenVanPhuocHau-19130066@users.noreply.github.com>
2023-02-17 15:07:09 +0700

    up framework
    khung project

---
+++ src/test/java/vn/edu/hcmuaf/fit/ecommerceclothingbackend/EcommerceClothingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmuaf.fit.ecommerceclothingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceClothingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
